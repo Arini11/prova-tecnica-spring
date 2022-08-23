5c01be414f049dcff22177becf8dedb5451a0373
Arini11 <elarnaumc@gmail.com>
2022-08-23 13:36:58 +0200

    no he guardat abans de fer push. magnífic...

--- src/main/java/me/arnaumas/controller/GroupMembershipController.java
+++ src/main/java/me/arnaumas/controller/GroupMembershipController.java
@@ -1,8 +1,11 @@
 package me.arnaumas.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.arnaumas.dto.GroupMembership;
+import me.arnaumas.dto.Parties;
 import me.arnaumas.service.GroupMembershipServiceImpl;
 
 @RestController
@@ -20,6 +24,10 @@ public class GroupMembershipController {
 	@Autowired
 	GroupMembershipServiceImpl groupMembershipServiceImpl;
 	
+	@GetMapping("/groupMemberships")
+	public List<GroupMembership> getAll(){
+		return groupMembershipServiceImpl.getAll();
+	}
 	@PostMapping("/groupMemberships")
 	public GroupMembership joinGroup(@RequestBody GroupMembership groupMembership) {
 		return groupMembershipServiceImpl.joinGroup(groupMembership);
