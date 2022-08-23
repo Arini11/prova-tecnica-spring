658f00a74c4516fdece7e781eb128a6ff4bb3208
Arini11 <elarnaumc@gmail.com>
2022-08-23 13:24:18 +0200

    getAll group membership

--- src/main/java/me/arnaumas/service/GroupMembershipServiceImpl.java
+++ src/main/java/me/arnaumas/service/GroupMembershipServiceImpl.java
@@ -1,5 +1,7 @@
 package me.arnaumas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +23,10 @@ public class GroupMembershipServiceImpl implements IGroupMembershipService {
 	public void leaveGroup(Integer id) {
 		groupMembershipDAO.deleteById(id);
 		
+	}
+
+	public List<GroupMembership> getAll() {
+		return groupMembershipDAO.findAll();
 	}
 
 }
--- src/main/java/me/arnaumas/service/IGroupMembershipService.java
+++ src/main/java/me/arnaumas/service/IGroupMembershipService.java
@@ -1,8 +1,12 @@
 package me.arnaumas.service;
 
+import java.util.List;
+
 import me.arnaumas.dto.GroupMembership;
 
 public interface IGroupMembershipService {
+	public List<GroupMembership> getAll(); //getAll
+
 	public GroupMembership joinGroup(GroupMembership joinGroup);
 	public void leaveGroup(Integer id);
 }
